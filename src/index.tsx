7e4ca7ef0459376ec3453d80e1b20eb89d139ba8
Park donghyeon <emma415g@gmail.com>
2022-04-15 02:11:26 +0900

    Revert "router 적용"
    This reverts commit 346626b76d5a9a8e09aa531f9832c34cff4f1a0f.

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
-import './index.scss';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Failed to find the root element");
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout/>} >
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-        </Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
